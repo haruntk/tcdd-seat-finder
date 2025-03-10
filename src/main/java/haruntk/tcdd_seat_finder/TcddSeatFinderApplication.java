d4d1fcb017b1f694d47b8a1a46db2cb78d0f670b
Harun Taha Kepenek <hakukpnk@gmail.com>
2025-03-11 00:58:04 +0300

    project created.

---
+++ src/main/java/haruntk/tcdd_seat_finder/TcddSeatFinderApplication.java
@@ -0,0 +1,13 @@
+package haruntk.tcdd_seat_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcddSeatFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcddSeatFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/haruntk/tcdd_seat_finder/TcddSeatFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package haruntk.tcdd_seat_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcddSeatFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
